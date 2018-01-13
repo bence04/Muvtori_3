d2da83280fb08566c5f35631e10cabf2e57cc39d
bence04 <tothbence95@gmail.com>
2018-01-13 11:49:31 +0100

    format files

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,17 +1,16 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
-import { AlertController } from 'ionic-angular';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs/Rx';
-import 'rxjs/add/operator/map';
-import 'rxjs/add/operator/catch';
+import { Component } from "@angular/core";
+import { NavController } from "ionic-angular";
+import { AlertController } from "ionic-angular";
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs/Rx";
+import "rxjs/add/operator/map";
+import "rxjs/add/operator/catch";
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+  selector: "page-home",
+  templateUrl: "home.html"
 })
 export class HomePage {
-
   private goodAns = 0;
   private wrongAns = 0;
   private actualInd = 0;
@@ -19,8 +18,12 @@ export class HomePage {
   private data: any;
   private dataLength = 0;
 
-  constructor(public navCtrl: NavController, public http: HttpClient, public alertCtrl: AlertController) {
-    this.http.get('assets/img.json').subscribe(res => {
+  constructor(
+    public navCtrl: NavController,
+    public http: HttpClient,
+    public alertCtrl: AlertController
+  ) {
+    this.http.get("assets/img.json").subscribe(res => {
       this.data = this.shuffle(res);
       this.dataLength = Object.keys(this.data).length;
       this.loadPicture();
@@ -29,28 +32,30 @@ export class HomePage {
 
   shuffle(a) {
     for (let i = a.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [a[i], a[j]] = [a[j], a[i]];
+      const j = Math.floor(Math.random() * (i + 1));
+      [a[i], a[j]] = [a[j], a[i]];
     }
     return a;
   }
 
   loadPicture() {
-    if(this.actualInd == this.dataLength) {
+    if (this.actualInd == this.dataLength) {
       let percent = this.goodAns / this.dataLength * 100;
       let alert = this.alertCtrl.create({
-        title: 'Végeztél az összessel',
-        subTitle: Math.round(percent * 100) / 100 + '% a jó válaszaid aránya.',
-        buttons: [{
-          text: 'Újra',
-          handler: () => {
-            this.goodAns = 0;
-            this.wrongAns = 0;
-            this.data = this.shuffle(this.data);
-            this.actualInd = 0;
-            this.loadPicture();
+        title: "Végeztél az összessel",
+        subTitle: Math.round(percent * 100) / 100 + "% a jó válaszaid aránya.",
+        buttons: [
+          {
+            text: "Újra",
+            handler: () => {
+              this.goodAns = 0;
+              this.wrongAns = 0;
+              this.data = this.shuffle(this.data);
+              this.actualInd = 0;
+              this.loadPicture();
+            }
           }
-        }]
+        ]
       });
       alert.present();
     } else {
@@ -68,7 +73,7 @@ export class HomePage {
       message: this.data[this.actualInd].author,
       buttons: [
         {
-          text: 'Tudtam 😀',
+          text: "Tudtam 😀",
           handler: () => {
             this.goodAns++;
             this.actualInd++;
@@ -76,7 +81,7 @@ export class HomePage {
           }
         },
         {
-          text: 'Nem tudtam 😞',
+          text: "Nem tudtam 😞",
           handler: () => {
             this.wrongAns++;
             this.actualInd++;
@@ -87,5 +92,4 @@ export class HomePage {
     });
     alert.present();
   }
-
 }
